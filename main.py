50a04d1b98a1449b94dac5e008a52e434322e874
CASCO <ERNESTO.CASCO@pepsico.com>
2023-10-05 16:12:36 -0600

    First approach efficient and not efficient done

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import torch
 import matplotlib.pyplot as plt
 
 """
-0 = floor tom
+0 = starting and/or finishing token
 1 = snare
 2 = closed hihat
 3 = ride cymbal
@@ -21,16 +21,15 @@ import matplotlib.pyplot as plt
 15 = close hihat (hihat foot)
 16 = floor tom and open hihat
 17 = snare and open hihat
-18 = <S>
-19 = <E>
+18 = floor tom
 """
 # open json file
 with open("tabs.json") as f:
     songs = json.load(f)
 
 # initialize our ocurrencies matrix (where we store each tuple of consequent characters)
-N = torch.zeros((20, 20), dtype=torch.int32)
-chars = [num for num in range(20)]
+N = torch.zeros((19, 19), dtype=torch.int32)
+chars = [num for num in range(19)]
 
 for song in songs:
     for ch1, ch2 in zip(song, song[1:]):
@@ -39,25 +38,58 @@ for song in songs:
 
 plt.figure(figsize=(16, 16))
 plt.imshow(N, cmap="Blues")
-for i in range(20):
-    for j in range(20):
+for i in range(19):
+    for j in range(19):
         plt.text(j, i, (str(j) + "," + str(i)), ha="center", va="bottom", color="gray")
         plt.text(j, i, N[i, j].item(), ha="center", va="top", color="gray")
 plt.axis("off")
-plt.show()
 
+
+# FIRST APPROACH WITH LITTLE ACCURACY
+def first_approach():
+    p = N[0].float()
+    p = p / p.sum()
+
+    g = torch.Generator().manual_seed(2147483647)
+    ix = 0
+    new_tab = [0]
+    for _ in range(10):
+        while True:
+            p = N[ix].float()
+            p = p / p.sum()
+            ix = torch.multinomial(
+                p, num_samples=1, replacement=True, generator=g
+            ).item()
+            new_tab.append(ix)
+            if ix == 0:
+                break
+        print(new_tab)
+        new_tab = []
+
+
+# MAKING P MORE EFFICIENT BY CALCULATING PROBABILITY DIST. BEFOREHAND
+def first_approach_efficient():
+    P = N.float()
+    P = P / P.sum(1, keepdim=True)
+
+    g = torch.Generator().manual_seed(2147483647)
+    ix = 0
+    new_tab = [0]
+    for _ in range(10):
+        while True:
+            p = P[ix]
+            ix = torch.multinomial(
+                p, num_samples=1, replacement=True, generator=g
+            ).item()
+            new_tab.append(ix)
+            if ix == 0:
+                break
+        print(new_tab)
+        new_tab = []
+
+
+first_approach_efficient()
 """
-p = N[0].float()
-p = p / p.sum()
-
-g = torch.Generator().manual_seed(2147483647)
-ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
-
-g = torch.Generator().manual_seed(2147483647)
-p = torch.rand(3, generator=g)
-p = p / p.sum()
-
-torch.multinomial(p, num_samples=100, replacement=True, generator=g)
 
 P = (N + 1).float()
 P /= P.sum(1, keepdims=True)
